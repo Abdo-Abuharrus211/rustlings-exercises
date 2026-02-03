c9b52639c2d5b355660e723d71770f43d5f92eca
Abdo-Abuharrus211 <aabuharrus@gmail.com>
2026-02-02 21:43:59 -0800

    Completed enums, structs, and strings modules

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Beginning my Rustlings exercises.
 
 - 29 Jan 2026: Completed sections 1 - 3
-- 30 Jan 2026: Completed sections 4 - 7 including quiz 1
-
+- 30 Jan 2026: Completed sections 4 - 6 including quiz 1
+- 2 Feb 2026: Completed sections 7 - 9
--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,20 +1,24 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let mut greet = String::from(input);
+    greet.push_str(" world!");
+    greet
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
     // You can optionally experiment here.
 }
 
-#[cfg(test)]
 mod tests {
     use super::*;
 
