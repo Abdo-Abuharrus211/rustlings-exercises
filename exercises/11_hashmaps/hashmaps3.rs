51788c920b2c7c1d845e195eae708dad78a53600
abdu <aabuharrus@gmail.com>
2026-02-08 15:43:04 -0800

    Implement elegant solution

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -24,8 +24,6 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // NOTE: We use `unwrap` because we didn't deal with error handling yet.
         let team_1_name = split_iterator.next().unwrap();
         let team_2_name = split_iterator.next().unwrap();
-        scores.entry(team_1_name).or_default();
-        scores.entry(team_2_name).or_default();
         let team_1_score: u8 = split_iterator.next().unwrap().parse().unwrap();
         let team_2_score: u8 = split_iterator.next().unwrap().parse().unwrap();
 
@@ -34,26 +32,17 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
 
-        if scores.contains_key(team_1_name) {
-            let new_score = TeamScores {
-                goals_scored: scores.get(team_1_name).unwrap().goals_scored + team_1_score,
-                goals_conceded: scores.get(team_1_name).unwrap().goals_conceded + team_2_score,
-            };
-            scores.insert(team_1_name, new_score);
-        }
+        let t1 = scores.entry(team_1_name).or_default();
+        t1.goals_scored += team_1_score;
+        t1.goals_conceded += team_2_score;
 
-        if scores.contains_key(team_2_name) {
-            let new_score = TeamScores {
-                goals_scored: scores.get(team_2_name).unwrap().goals_scored + team_2_score,
-                goals_conceded: scores.get(team_2_name).unwrap().goals_conceded + team_1_score,
-            };
-            scores.insert(team_2_name, new_score);
-        }
+        let t2 = scores.entry(team_2_name).or_default();
+        t2.goals_scored += team_2_score;
+        t2.goals_conceded += team_1_score;
     }
 
     scores
 }
-
 
 fn main() {
     // You can optionally experiment here.
