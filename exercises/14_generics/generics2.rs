f71a5c9381509da76ffe1ce488249b4e56117ad8
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2026-02-26 21:46:49 -0800

    Completed Generics section

--- exercises/14_generics/generics2.rs
+++ exercises/14_generics/generics2.rs
@@ -1,18 +1,24 @@
 // This powerful wrapper provides the ability to store a positive integer value.
 // TODO: Rewrite it using a generic so that it supports wrapping ANY type.
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T,
 }
 
 // TODO: Adapt the struct's implementation to be generic over the wrapped value.
-impl Wrapper {
-    fn new(value: u32) -> Self {
+impl<T> Wrapper<T> {
+    fn new(value: T) -> Self {
         Wrapper { value }
     }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let w =  Wrapper{value:9};
+    println!("This wrapper has a value of {}", w.value);
+
+    let y =  Wrapper{value:-2.7};
+    println!("This wrapper has a value of {}", y.value);
+    
 }
 
 #[cfg(test)]
